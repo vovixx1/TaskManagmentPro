a720e2c644f942aab2df127a2ec82cdeb89f9ba9
Vova <baglykvova11@gmail.com>
2024-12-02 14:15:23 +0100

    sdax

---
+++ TaskManagerPro/users/views.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
